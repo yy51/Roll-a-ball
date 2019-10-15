514ac61807036672442873b6a959be54ae00c41e
Yamauchi Yuta <52147503+yy51@users.noreply.github.com>
2019-10-15 13:05:29 +0900

    unityチュートリアル「カメラを動かす」あたりまで

---
+++ Assets/Script/FollowPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform target; //ターゲットへの参照
+
+    // Update is called once per frame
+    void Update()
+    {
+        //自分の座標にtargetの座標を代入する
+        GetComponent<Transform>().position = target.position;
+    }
+}
